08c9dacd4e4839c02d993c3e9b0f5cefb4875c9f
[SW4/2반] 김종운 <developer.cdd@gmail.com>
2023-06-16 21:42:03 +0000

    Update AnswerPage.tsx

--- src/pages/AnswerPage/AnswerPage.tsx
+++ src/pages/AnswerPage/AnswerPage.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AnswerRender from "./AnswerRender";
 import styled from "styled-components";
 import FooterImage from "../MainPage/FooterImage";
 
 const AnswerPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <AnswerRender />
